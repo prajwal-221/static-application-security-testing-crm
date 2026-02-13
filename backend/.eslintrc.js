1e2c2c5017e99c09bbb0165e4b1142bf9d809321
prajwal-221 <prajwaln@heaptrace.com>
2026-02-13 19:50:17 +0530

    add: ESLint Setup Stage 1.5 Task:SL01

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,15 +1,23 @@
 module.exports = {
   env: {
-    browser: true,
-    es2020: true,
     node: true,
+    es2021: true,
   },
-  extends: 'eslint:recommended',
+  extends: [
+    "eslint:recommended",
+    "plugin:security/recommended"
+  ],
+  plugins: ["security"],
   parserOptions: {
-    ecmaVersion: 12,
+    ecmaVersion: 2021,
     sourceType: 'module',
   },
   rules: {
     'no-console': 0,
-  },
+    // Security rules
+    "security/detect-object-injection": "warn",
+    "security/detect-non-literal-fs-filename": "warn",
+    "security/detect-child-process": "error",
+    "security/detect-eval-with-expression": "error"
+  }
 };
