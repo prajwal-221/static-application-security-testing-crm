03fc2809899a43ebe120a46b71d589fbc88dcc7a
prajwal-221 <prajwaln@heaptrace.com>
2026-02-13 13:54:34 +0530

    update: jenkins.security file for jenkins DevSecops pipeline

--- jenkins/jenkins-startup-script.sh
+++ jenkins/jenkins-startup-script.sh
@@ -1,9 +1,13 @@
 docker build -t jenkins-devsecops:lts -f Dockerfile .
 
+docker stop jenkins || true
+docker rm jenkins || true
+
 docker run -d \
   --name jenkins \
   --restart unless-stopped \
   --network host \
+  --group-add $(getent group docker | cut -d: -f3) \
   -v /var/lib/jenkins:/var/jenkins_home \
   -v ~/.kube:/var/jenkins_home/.kube \
   -v ~/.minikube:/home/ht-admin/.minikube \
